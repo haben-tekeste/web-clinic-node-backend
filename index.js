4720957e42d8177b72575b24375d2fd441e1eda0
Haben Tekeste <habu_@Habens-MacBook-Air.local>
2022-11-15 19:30:53 +0400

    path module bug fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("./src/models/User");
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
